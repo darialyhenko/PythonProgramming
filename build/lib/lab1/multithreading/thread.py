b580eb97aad2cbb7bb25cb43f4b0a28b49109412
Daria Lyhenko <daria.lyhenko@gmail.comgit  config --global user.name Lyhenko D>
2022-05-09 23:34:07 +0600

    lr4

---
+++ build/lib/lab1/multithreading/thread.py
@@ -0,0 +1,17 @@
+from concurrent.futures import ThreadPoolExecutor
+import requests
+
+def get_session():
+    return requests.Session()
+
+def download_site(url):
+    session = get_session()
+    with session:
+        with session.get(url) as response:
+            print(f"Read {len(response.content)} from {url}")
+            return f"Read {len(response.content)} from {url}"
+
+def check_sites(sites):
+    with ThreadPoolExecutor(max_workers=20) as thread:
+        res = thread.map(download_site, sites)
+        return list(res)
